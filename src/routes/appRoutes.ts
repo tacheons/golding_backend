27d23a32630a6f02abfdd6a244e47db8846da014
Fabian Madueke <fabtronss@gmail.com>
2024-11-28 19:17:20 +0100

    Add app route file

---
+++ src/routes/appRoutes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import userRoutes from "./userRoutes";
+import authRoutes from "./authRoutes";
+
+/**
+ * @implementaionNote :
+ * appRoutes is a route component that is an aggregator route,
+ * it combines all the routes in the application
+ * and integrate them collectively into the application.
+ *
+ * @return
+ * approutes
+ *
+ * @author & @contributors
+ * fabian Madueke
+ *
+ * @since
+ * 1.0.0
+ */
+let appRoutes = Router();
+
+appRoutes.use([authRoutes, userRoutes]);
+
+export default appRoutes;
