30ac344fc25cef37976bf23704286175d79fd567
Fabian Madueke <fabtronss@gmail.com>
2024-11-29 16:39:46 +0100

    Authentication Service fully implemented

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -2,6 +2,18 @@ import { AppDataSource } from "../data-source";
 import { NextFunction, Request, Response } from "express";
 import { User } from "../entity/User";
 
+/**
+ * @implementaion
+ * This is the Authentication Service Layer of the application that services
+ * all Authentication Controller functions
+ *
+ * @return
+ * returns client authentication data
+ *
+ * @author
+ * @since
+ * v1.0.0
+ */
 export class AuthService {
   private userRepository: any;
 
@@ -9,6 +21,18 @@ export class AuthService {
     this.userRepository = AppDataSource.getRepository(User);
   }
 
+  /**
+   * @implementaion
+   * This method service the user login, by fetching the required data 
+   * from the database
+   *
+   * @return
+   * returns client authentication data
+   *
+   * @author
+   * @since
+   * v1.0.0
+   */
   async login(email: String, password: String) {
     const user = await this.userRepository.findOne({
       where: { email },
