d76a9db423e729bc4318a7c1babc91d9997222d3
Fabian Madueke <fabtronss@gmail.com>
2024-11-29 16:43:25 +0100

    Server configuration fully implemented

--- src/modules/configurations/serverConfig.ts
+++ src/modules/configurations/serverConfig.ts
@@ -1,5 +1,13 @@
 import { url } from "inspector";
 
+/**
+ * @implementation :
+ * Configuration file for the server
+ *
+ * @author
+ * @since
+ * v1.0.0
+ */
 let serverConfig = {
   port: 4000,
   url: "http://localhost",
