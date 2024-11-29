cdcf75f64ec4d3919a781c3ddd944ce24ae9e490
Fabian Madueke <fabtronss@gmail.com>
2024-11-29 16:47:29 +0100

    User Entity fully implemeted

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -11,6 +11,20 @@ import {
   isEmail,
 } from "class-validator";
 
+/**
+ * @implementaion
+ * User Entity Configuration, use by the ORM to
+ * map data with database, the decorators are use for constrains
+ * of the data to ensure data integrity
+ *
+ * @return
+ * an array of services
+ *
+ * @author
+ * @since
+ * v1.0.0
+ */
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
